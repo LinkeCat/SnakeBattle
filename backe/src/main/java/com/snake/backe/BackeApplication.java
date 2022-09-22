097d18bde11ac87cfa3e7052cc2d6e464e14314a
TEX <1071945332@qq.com>
2022-09-23 00:43:52 +0800

    first commit!

---
+++ backe/src/main/java/com/snake/backe/BackeApplication.java
@@ -0,0 +1,13 @@
+package com.snake.backe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackeApplication.class, args);
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+hello world!
+# SnakeBattle
+# SnakeBattle
+# SnakeBattle
